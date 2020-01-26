cc2a28008b8637784b1033813b1fd9b9912aed9a
Hendi Santika <hendisantika@gmail.com>
2020-01-27 05:47:21 +0700

    #3 - Add data.sql file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO Address (addid,country,city,FLAT) VALUES ('13', 'Indonesia', 'Jakarta','r20');
+INSERT INTO Address (addid,country,city,FLAT) VALUES ('14', 'USA', 'San Fransisco','ad20');
+INSERT INTO Address (addid,country,city,FLAT) VALUES ('15', 'China', 'Wuhan','a221');
+INSERT INTO Employee (id, name, age,addid) VALUES ('1', 'employee1', 23,'13');
+INSERT INTO Employee (id, name, age,addid) VALUES ('3', 'employee1', 53,'14');
+INSERT INTO Employee (id, name, age,addid) VALUES ('2', 'employee1', 22,'15');
