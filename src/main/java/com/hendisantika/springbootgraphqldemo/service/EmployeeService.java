a580df193fb5bbaf04c68f13e31f5277cdd31756
Hendi Santika <hendisantika@gmail.com>
2020-01-27 05:56:25 +0700

    #12 - Add loadSchema service in EmployeeService

--- src/main/java/com/hendisantika/springbootgraphqldemo/service/EmployeeService.java
+++ src/main/java/com/hendisantika/springbootgraphqldemo/service/EmployeeService.java
@@ -2,10 +2,19 @@ package com.hendisantika.springbootgraphqldemo.service;
 
 import com.hendisantika.springbootgraphqldemo.repository.EmployeeRepository;
 import graphql.GraphQL;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,4 +34,32 @@ public class EmployeeService {
     Resource resource;
 
     private GraphQL graphQL;
+
+    @Autowired
+    private AllEmployeeDataFetcher allEmployeeDataFetcher;
+
+    // load schema at application start up
+    @PostConstruct
+    private void loadSchema() throws IOException {
+
+
+        // get the schema
+        File schemaFile = resource.getFile();
+        // parse schema
+        TypeDefinitionRegistry typeRegistry = new SchemaParser().parse(schemaFile);
+        RuntimeWiring wiring = buildRuntimeWiring();
+        GraphQLSchema schema = new SchemaGenerator().makeExecutableSchema(typeRegistry, wiring);
+        graphQL = GraphQL.newGraphQL(schema).build();
+    }
+
+    private RuntimeWiring buildRuntimeWiring() {
+        return RuntimeWiring.newRuntimeWiring()
+                .type("Query", typeWiring -> typeWiring
+                        .dataFetcher("allEmployee", allEmployeeDataFetcher))
+                .build();
+    }
+
+    public GraphQL getGraphQL() {
+        return graphQL;
+    }
 }
